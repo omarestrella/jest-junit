8847a42f19dec21f609c85dcb0f10dfe364cea05
Omar Estrella <oyestrella91@gmail.com>
2021-04-02 16:11:49 -0400

    Add test case properties

--- junitProperties.js
+++ junitProperties.js
@@ -2,6 +2,11 @@ module.exports = (suite) => {
   return {
     suite: {
       'best-tester': 'Jason Palmer'
+    },
+    testCases: {
+      'should bar': {
+        'the-property': 'property-value'
+      }
     } 
   }
 }
